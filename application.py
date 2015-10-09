8216cb51decd6150a9c074207e33d9095211d490
Javie1998 <floresjavier251@gmail.com>
2015-10-09 09:16:01 -0600

    program with test

--- application.py
+++ application.py
@@ -1,30 +1,37 @@
+"""this program will help the user to calculate the factorial
+ of a number, and it will ask the user
+  if wants insert other number to calculate"""
 import math
 import sys
 import os
 
 def ing():
-	try:
-		print "Calculating factorial of a number"
-		num = input ("insert a number to calculate \n")
-		if num == int(num):
-			print "the factorial of ", num, "is", math.factorial(num)
-	except NameError:
-		print "you have to insert only numbers"
-		raw_input("press enter to continue")
-	again()
+    """this function will ask the user for the number that he/she wants to calculate"""
+    try:
+        print "Calculating factorial of a number"
+        num = raw_input("insert a number to calculate \n")
+        if num == int(num):
+            print "the factorial of ", num, "is", math.factorial(num)
+    except NameError:
+        print "you have to insert only numbers"
+        raw_input("press enter to continue")
+    again()
 
 def again():
-	other = raw_input("Do you want to insert any other number? y/n \n")
-	if other == "y" or other == "yes":
-	    clear()
-	    ing()
-	elif other == "n" or other == "no":
-		exit()    
+    """in this part the program will ask if the user wants to insert other number"""
+    other = raw_input("Do you want to insert any other number? y/n \n")
+    if other == "y" or other == "yes":
+        clear()
+        ing()
+    elif other == "n" or other == "no":
+        out()
 
 def clear():
-	os.system("cls")
+    """it only clear"""
+    os.system("cls")
 
-def exit():	 
-	raw_input("press enter to continue")   
-	sys.exit()
-ing()	
+def out():
+    """and it serves to go out of the program"""
+    raw_input("press enter to continue")
+    sys.exit()
+ing()
